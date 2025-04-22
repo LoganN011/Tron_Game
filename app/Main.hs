36e9875af8c0dd47ca73c18658d57e058d041e81
Logan Nunno <lnunno1@unm.edu>
2025-04-21 19:22:15 -0600

    made some helper stuff and set the basic idea for what the state will look like

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,31 @@ module Main where
 import Brillo
 import Brillo.Interface.IO.Game (Event (EventKey), Key (Char))
 
+data Direction = UP | DOWN  | LEFT  | RIGHT 
+data Player = AI | Human Direction Pos 
+type Pos = (Int,Int)
+
+move :: Direction -> Pos -> Pos
+move UP (x, y) = (x, y+1)
+move DOWN (x, y) = (x, y-1)
+move RIGHT  (x, y) = (x+1, y)
+move LEFT  (x, y) = (x-1, y)
+
+
+
+size = 500
+
+
 data State =
   MkState { 
+    visted :: [[Bool]],
+    players :: [Player],
+    grid :: [[Pos]]
       
-    }deriving(Eq,Show)
+    } -- deriving(Eq,Show)
 
 initState:: State
-initState = MkState 
+initState = undefined
 
 
 stateToPicture:: State -> Picture
