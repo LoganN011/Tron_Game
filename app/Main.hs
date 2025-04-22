985594fe0d6f297e439fea1910b00357540a1cfe
Logan Nunno <lnunno1@unm.edu>
2025-04-21 18:54:16 -0600

    Base structure for the methods to make the picture on the screen

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,37 @@
+
 module Main where
 import Brillo
+import Brillo.Interface.IO.Game (Event (EventKey), Key (Char))
+
+data State =
+  MkState { 
+      
+    }deriving(Eq,Show)
+
+initState:: State
+initState = MkState 
+
+
+stateToPicture:: State -> Picture
+stateToPicture s = undefined
+
+stateToState:: State -> State
+stateToState s = undefined
+
+eventUpdate::Event-> State -> State 
+eventUpdate (EventKey (Char c) _ _ _) s = case c of 
+                                            'w' -> initState
+                                            _ -> initState
+                                            
+eventUpdate _ s = initState
+
+
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = play FullScreen
+  black
+  60
+  initState
+  stateToPicture
+  eventUpdate
+  (\ f -> stateToState)
