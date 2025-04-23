c87c1cf675cbfbe8654ca2505fa0c178a5469431
Logan Nunno <lnunno1@unm.edu>
2025-04-22 18:53:25 -0600

    Dot moving across the screen. Need to add the stuff for the path in stateToState

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,9 @@ module Main where
 import Brillo
 import Brillo.Interface.IO.Game (Event (EventKey), Key (Char))
 
-data Direction = UP | DOWN  | LEFT  | RIGHT 
-data Player = AI Pos | Human Direction Pos 
-type Pos = (Int,Int)
+data Direction = UP | DOWN  | LEFT  | RIGHT
+data Player = AI Pos | Human Direction Pos
+type Pos = (Float,Float)
 
 move :: Direction -> Pos -> Pos
 move UP (x, y) = (x, y+1)
@@ -13,23 +13,28 @@ move DOWN (x, y) = (x, y-1)
 move RIGHT  (x, y) = (x+1, y)
 move LEFT  (x, y) = (x-1, y)
 
+aiMove :: Direction -> Pos -> Pos
+aiMove = move
+
 
 
 size = 500
 
 
 data State =
-  MkState { 
-    visted :: [[Bool]], -- maybe make this a list of POS so I can draw the line easier or have both
+  MkState {
+    --visted :: [[Bool]], -- maybe make this a list of POS so I can draw the line easier or have both
+    visted :: [Pos],
     players :: [Player],
     grid :: [[Pos]]
-      
+
     } -- deriving(Eq,Show)
 
 initState:: State
 initState = MkState
-            (replicate 500 (replicate 500 False))
-            [AI (-200,-200) ,Human LEFT (200,200)]
+            []
+            --(replicate 500 (replicate 500 False))
+            [Human LEFT (200,200)]
             [[]] -- make method to fill in all of the postions based on the size or something similar. 
             -- might not need if I just do the postions visted list thing
 
@@ -37,25 +42,36 @@ initState = MkState
 
 
 stateToPicture:: State -> Picture
-stateToPicture s = undefined
+stateToPicture s =pictures [ pictures (go (players s)), color white (line (visted s))]
+  where
+    go [] = []
+    go (Human _ (x,y):rest) = Color white (Polygon [(x + dx, y + dy) | dx <- [-1..1], dy <- [-1..1]]) : go rest
+    go (AI (x,y):rest) = Color white (Polygon [(x + dx, y + dy) | dx <- [-1..1], dy <- [-1..1]]) : go rest
 
 stateToState:: State -> State
-stateToState s = undefined
+stateToState s = s {players = go (players s)}
+  where
+    go [] = []
+    go (Human d p:rest) = Human d (move d p) : go rest -- Find some way of adding the postion to the visted list once it is moved it 
+    go (AI p:rest) = AI (aiMove RIGHT p) :rest  -- make do computer movement method 
 
-eventUpdate::Event-> State -> State 
-eventUpdate (EventKey (Char c) _ _ _) s = case c of 
+eventUpdate::Event-> State -> State
+eventUpdate (EventKey (Char c) _ _ _) s = case c of
                                             'w' -> initState
+                                            'a' -> initState
+                                            's' -> initState
+                                            'd' -> initState
                                             _ -> initState
-                                            
 eventUpdate _ s = initState
 
 
 
+
 main :: IO ()
-main = play FullScreen
+main = play FullScreen--(InWindow "Tron" (100,100) (size,size))
   black
   60 -- frame rate so maybe lower if it moves too fast or something im not sure
   initState
   stateToPicture
   eventUpdate
-  (\ f -> stateToState)
+  (\f -> stateToState)
