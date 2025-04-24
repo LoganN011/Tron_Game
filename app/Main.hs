59d0dd214d2aac5e0f9ee6b82e13def71f6adc09
Logan Nunno <lnunno1@unm.edu>
2025-04-24 08:34:44 -0600

    added direction handling of input and also drawing the line for a single player

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 
 module Main where
 import Brillo
-import Brillo.Interface.IO.Game (Event (EventKey), Key (Char))
+import Brillo.Interface.IO.Game (Event (EventKey), Key (Char), KeyState (Up))
 
 data Direction = UP | DOWN  | LEFT  | RIGHT
 data Player = AI Pos | Human Direction Pos
@@ -24,7 +24,7 @@ size = 500
 data State =
   MkState {
     --visted :: [[Bool]], -- maybe make this a list of POS so I can draw the line easier or have both
-    visted :: [Pos],
+    visited :: [Pos],
     players :: [Player],
     grid :: [[Pos]]
 
@@ -42,28 +42,49 @@ initState = MkState
 
 
 stateToPicture:: State -> Picture
-stateToPicture s =pictures [ pictures (go (players s)), color white (line (visted s))]
+stateToPicture s =pictures [ pictures (go (players s)), color white (line (visited s))]
   where
     go [] = []
     go (Human _ (x,y):rest) = Color white (Polygon [(x + dx, y + dy) | dx <- [-1..1], dy <- [-1..1]]) : go rest
     go (AI (x,y):rest) = Color white (Polygon [(x + dx, y + dy) | dx <- [-1..1], dy <- [-1..1]]) : go rest
 
 stateToState:: State -> State
-stateToState s = s {players = go (players s)}
-  where
-    go [] = []
-    go (Human d p:rest) = Human d (move d p) : go rest -- Find some way of adding the postion to the visted list once it is moved it 
-    go (AI p:rest) = AI (aiMove RIGHT p) :rest  -- make do computer movement method 
+stateToState s = s { players = newPlayers, visited = visited s ++ newVisited }
+ where
+    (newPlayers, newVisited) = go (players s)
+
+    go [] = ([], [])
+    go (Human d p : rest) =
+      let newPos = move d p
+          (restPlayers, restVisited) = go rest
+      in (Human d newPos : restPlayers, newPos : restVisited)
+    go (AI p : rest) =
+      let newPos = aiMove RIGHT p
+          (restPlayers, restVisited) = go rest
+      in (AI newPos : restPlayers, newPos : restVisited)
+
+
+-- stateToState s = s {players = go (players s)}
+--   where
+--     go [] = []
+--     go (Human d p:rest) = Human d (move d p) : go rest -- Find some way of adding the postion to the visted list once it is moved it 
+--     go (AI p:rest) = AI (aiMove RIGHT p) :rest  -- make do computer movement method 
 
 eventUpdate::Event-> State -> State
 eventUpdate (EventKey (Char c) _ _ _) s = case c of
-                                            'w' -> initState
-                                            'a' -> initState
-                                            's' -> initState
-                                            'd' -> initState
+                                            'w' -> s { players = updateHumanDirection UP s} 
+                                            'a' -> s { players = updateHumanDirection LEFT s} 
+                                            's' -> s { players = updateHumanDirection DOWN s} 
+                                            'd' -> s { players = updateHumanDirection RIGHT s} 
                                             _ -> initState
 eventUpdate _ s = initState
 
+
+updateHumanDirection :: Direction-> State  -> [Player]
+updateHumanDirection d s = foldr go [] (players s)
+  where
+    go (Human _ p) recur = Human d p : recur
+    go ai recur = ai : recur
 
 
 
